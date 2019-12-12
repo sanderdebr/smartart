ac5e4108c705fde4a4ea5d980b1dfba9bd3a6102
sanderdebr <sander606@gmail.com>
2019-12-12 09:30:36 +0100

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,1 @@
-const h1 = document.querySelector('h1');
-
-h1.style.color = 'red';
+import '../css/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,19 @@ module.exports = {
             template: './src/index.html'
         })
     ],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+        ],
+    },
 };
